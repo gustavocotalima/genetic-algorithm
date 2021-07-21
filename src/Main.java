46943964771a4d7d21c55fc44020ddf3dcb34667
gustavocotalima <gustavolima@unifei.edu.br>
2021-07-21 15:56:48 -0300

    add FileWriter

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.Scanner;
@@ -33,9 +35,9 @@ public class Main {
     }
 
     public static int escolherProgenitor(int tamPopulacao){
-        int prob = ThreadLocalRandom.current().nextInt(0,tamPopulacao*((tamPopulacao+1)/2));
+        int prob = ThreadLocalRandom.current().nextInt(0,1+(tamPopulacao*((tamPopulacao+1)/2)));
 
-        for (int i = tamPopulacao, j = 0, soma = 0; i > 0; i--, j++) {
+        for (int i = tamPopulacao, j = 0, soma = 0; i >= 0; i--, j++) {
             soma += i;
             if (soma > prob)
                 return j;
@@ -43,36 +45,23 @@ public class Main {
         return 0;
     }
 
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int tamPopulacao, numGeracao;
-        double taxaMutacao;
+    public static void run (FileWriter arquivo, int tamPopulacao, int numGeracao, double taxaMutacao, int execucao) throws IOException {
+        double[] media = new double[numGeracao];
 
-        System.out.print("Insira o tamanho da população: ");
-        tamPopulacao = scn.nextInt();
-
-        System.out.print("Insira o número de gerações: ");
-        numGeracao = scn.nextInt();
-
-        System.out.print("Insira a taxa de mutacao (0 - 100): ");
-        taxaMutacao = scn.nextDouble();
-
-        double[]  media = new double[numGeracao];
-
-        ArrayList<ArrayList<Individuo>> individuos = new ArrayList<ArrayList<Individuo>>(numGeracao);
-        ArrayList<Individuo> primeiraGeracao = new ArrayList<Individuo>(tamPopulacao);
+        ArrayList<ArrayList<Individuo>> individuos = new ArrayList<>(numGeracao);
+        ArrayList<Individuo> primeiraGeracao = new ArrayList<>(tamPopulacao);
 
 
-        //criação da primeira geração
+        //primeira geração
 
         double soma = 0;
         for (int i = 0; i < tamPopulacao; i++) {
-            double x1 = ThreadLocalRandom.current().nextInt(-100000, 100000)/10000.0;
-            double x2 = ThreadLocalRandom.current().nextInt(-100000, 100000)/10000.0;
-            primeiraGeracao.add(new Individuo(x1,x2));
+            double x1 = ThreadLocalRandom.current().nextInt(-100000, 100001) / 10000.0;
+            double x2 = ThreadLocalRandom.current().nextInt(-100000, 100001) / 10000.0;
+            primeiraGeracao.add(new Individuo(x1, x2));
             soma += primeiraGeracao.get(i).getFitness();
         }
-        media[0]=soma/tamPopulacao;
+        media[0] = soma / tamPopulacao;
 
         primeiraGeracao.sort(Individuo::compareTo);
 
@@ -81,8 +70,8 @@ public class Main {
         //outras gerações
 
         for (int i = 1; i < numGeracao; i++) {
-            ArrayList<Individuo> geracaoIndividuos = new ArrayList<Individuo>(tamPopulacao);
-            geracaoIndividuos.add(individuos.get(i-1).get(0));
+            ArrayList<Individuo> geracaoIndividuos = new ArrayList<>(tamPopulacao);
+            geracaoIndividuos.add(individuos.get(i - 1).get(0));
 
             soma = 0;
 
@@ -92,19 +81,48 @@ public class Main {
 
                 do {
                     nMae = escolherProgenitor(tamPopulacao);
-                } while (nMae==nPai);
+                } while (nMae == nPai);
 
-                geracaoIndividuos.add(cruzamento(individuos.get(i-1).get(nPai),individuos.get(i-1).get(nMae),taxaMutacao));
+                geracaoIndividuos.add(cruzamento(individuos.get(i - 1).get(nPai), individuos.get(i - 1).get(nMae), taxaMutacao));
 
                 soma += geracaoIndividuos.get(j).getFitness();
             }
-            media[i]=soma/tamPopulacao;
+            media[i] = soma / tamPopulacao;
             geracaoIndividuos.sort(Individuo::compareTo);
             individuos.add(geracaoIndividuos);
         }
 
         for (int i = 0; i < numGeracao; i++) {
-            System.out.println("Melhor: "+individuos.get(i).get(0).getFitness()+"  Média: "+media[i]+"  Pior: "+individuos.get(i).get(tamPopulacao-1).getFitness());
+            System.out.println("Geração: "+(i+1)+" Melhor: " + individuos.get(i).get(0).getFitness() + "  Média: " + media[i] + "  Pior: " + individuos.get(i).get(tamPopulacao - 1).getFitness());
+            arquivo.write(execucao+" "+i+" "+individuos.get(i).get(0).getFitness() + " " + media[i] + " " + individuos.get(i).get(tamPopulacao - 1).getFitness() + "\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            FileWriter arquivo = new FileWriter("arquivo.txt"); //arquivo para gerar o gráfico no matlab
+            Scanner scn = new Scanner(System.in);
+
+            System.out.print("Insira o tamanho da população: ");
+            int tamPopulacao = scn.nextInt();
+
+            System.out.print("Insira o número de gerações: ");
+            int numGeracao = scn.nextInt();
+
+            System.out.print("Insira a taxa de mutação % (0 - 100): ");
+            double taxaMutacao = scn.nextDouble();
+
+            System.out.print("Insira o número de vezes para executar: ");
+            int exec = scn.nextInt();
+
+            for (int execucao = 1; execucao <= exec; execucao++) {
+                System.out.println("\nExecução "+execucao+"\n");
+                run(arquivo,tamPopulacao,numGeracao,taxaMutacao,execucao);
+            }
+
+            arquivo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
